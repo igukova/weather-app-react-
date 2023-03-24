8d7a1ca3deca70750ee113d6903d01e32b788985
irguk <igukova@gmail.com>
2023-03-24 13:03:33 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,25 @@
-body {
-    font-family: "Courier New", Courier, monospace;
-    background-image: url(./img/clouds.jpg);
+import React, { useState } from "react";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setMessage(`It is currently 20°C in ${city}`);
   }
-  div.container {
-    margin-left: 30px;
-    margin-top: 30px;
-    padding: 20px 0 40px 40px;
-    border: 1px solid grey;
-    max-width: 800px;
-    font-family: Poppins;
-    background: rgba(255, 255, 255, 0.37) 20%;
-    border-radius: 20px;
+
+  function changeCity(event) {
+    setCity(event.target.value);
   }
-  .typecit {
-    max-width: 600px;
-    text-decoration: underline;
-    text-align: center;
-  }
-  #current-temperature {
-    text-align: left;
-  }
-  .col-1 {
-    padding: 50px 10px;
-  }
-  .today {
-    margin: 20px 10px 20px 0;
-    padding: 20px 0 20px 10px;
-    max-width: 500px;
-    border: 1px solid grey;
-    border-radius: 40px;
-  }
-  .forecast {
-    padding: 30px 10px 0 0;
-    max-width: 600px;
-    text-align: center;
-  }
-  .highest {
-    font-weight: 500;
-  }
-  .temperature {
-    font-size: 36px;
-  }
-  .units {
-    font-size: 36px;
-  }
-  #farenheit-link {
-    color: aqua;
-  }
-  
-  .App {
-    max-width: 750px;
-    margin: 10px auto;
-  }
-  
+
+  return (
+    <div className="WeatherSearch">
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Type a city" onChange={changeCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <h2>{message}</h2>
+    </div>
+  );
+}
